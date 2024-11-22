39fa865140361acbe2de01f000c0b7811f4c3bd5
lazarclimber <lazarciricthemaze@hotmail.com>
2024-11-22 10:45:17 +0100

    Add project files.

---
+++ To-Do-List/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace To_Do_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.tasksListBox = new System.Windows.Forms.ListBox();
+            this.addTaskBtn = new System.Windows.Forms.Button();
+            this.removeTaskBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(619, 64);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To-Do-List";
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(139, 115);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(180, 29);
+            this.textBox.TabIndex = 2;
+            // 
+            // tasksListBox
+            // 
+            this.tasksListBox.BackColor = System.Drawing.SystemColors.Window;
+            this.tasksListBox.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tasksListBox.FormattingEnabled = true;
+            this.tasksListBox.ItemHeight = 37;
+            this.tasksListBox.Location = new System.Drawing.Point(104, 161);
+            this.tasksListBox.Name = "tasksListBox";
+            this.tasksListBox.Size = new System.Drawing.Size(442, 337);
+            this.tasksListBox.TabIndex = 3;
+            this.tasksListBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.TasksListBox_DrawItem);
+            this.tasksListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TasksListBox_MouseDoubleClick);
+            // 
+            // addTaskBtn
+            // 
+            this.addTaskBtn.Location = new System.Drawing.Point(336, 115);
+            this.addTaskBtn.Name = "addTaskBtn";
+            this.addTaskBtn.Size = new System.Drawing.Size(75, 29);
+            this.addTaskBtn.TabIndex = 4;
+            this.addTaskBtn.Text = "Add Task";
+            this.addTaskBtn.UseVisualStyleBackColor = true;
+            this.addTaskBtn.Click += new System.EventHandler(this.AddTaskBtn_Click);
+            // 
+            // removeTaskBtn
+            // 
+            this.removeTaskBtn.Location = new System.Drawing.Point(505, 115);
+            this.removeTaskBtn.Name = "removeTaskBtn";
+            this.removeTaskBtn.Size = new System.Drawing.Size(86, 29);
+            this.removeTaskBtn.TabIndex = 5;
+            this.removeTaskBtn.Text = "Remove task";
+            this.removeTaskBtn.UseVisualStyleBackColor = true;
+            this.removeTaskBtn.Click += new System.EventHandler(this.RemoveTaskBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(424, 115);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(75, 29);
+            this.editBtn.TabIndex = 6;
+            this.editBtn.Text = "Edit Task";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.EditBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 542);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.removeTaskBtn);
+            this.Controls.Add(this.addTaskBtn);
+            this.Controls.Add(this.tasksListBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "To-Do-List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ListBox tasksListBox;
+        private System.Windows.Forms.Button addTaskBtn;
+        private System.Windows.Forms.Button removeTaskBtn;
+        private System.Windows.Forms.Button editBtn;
+    }
+}
+
---
+++ To-Do-List/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace To_Do_List
+{
+    public partial class Form1 : Form
+    {
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private List<int> crossedOutItems = new List<int>();
+        private bool doubleClickHandled = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            #region MenuStrip
+            this.menuStrip1 = new MenuStrip();
+            this.fileToolStripMenuItem = new ToolStripMenuItem();
+            this.saveToolStripMenuItem = new ToolStripMenuItem();
+            this.loadToolStripMenuItem = new ToolStripMenuItem();
+
+            this.menuStrip1.Items.AddRange(new ToolStripItem[] 
+            {
+                this.fileToolStripMenuItem
+            });
+            this.menuStrip1.Location = new Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] 
+            {
+                this.saveToolStripMenuItem, this.loadToolStripMenuItem
+            });
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new EventHandler(this.SaveToolStripMenuItem_Click);
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new Size(98, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new EventHandler(this.LoadToolStripMenuItem_Click);
+
+            this.Controls.Add(menuStrip1);
+            #endregion
+        }
+
+
+        private void LoadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Text Files (*.txt)|*.txt";
+                string filePath = "";
+
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                filePath = dialog.FileName;
+
+                tasksListBox.Items.Clear();
+                    crossedOutItems.Clear();
+
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                           {
+                              tasksListBox.Items.Add(line);
+                           }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex, "Error message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Files (*.txt)|*.txt";
+            dialog.Title = "Save your tasks";
+            string filePath = "";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = dialog.FileName;
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    foreach (string item in tasksListBox.Items)
+                    {
+                        sw.WriteLine(item);
+                    }
+                    MessageBox.Show("File saved successfully", "Conformation message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }    
+        }
+
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                tasksListBox.Items.Add(textBox.Text);
+                textBox.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Please input a task.");
+            }
+        }
+
+        private void RemoveTask()
+        {
+            if (tasksListBox.SelectedItem != null)
+            {
+                tasksListBox.Items.Remove(tasksListBox.SelectedItem);
+                crossedOutItems.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Select a task that you want to remove.");
+            }
+        }
+
+        private void AddTaskBtn_Click(object sender, EventArgs e)
+        {
+            AddTask();
+        }
+
+        private void RemoveTaskBtn_Click(object sender, EventArgs e)
+        {
+            RemoveTask();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                RemoveTask();
+            }
+            else if(e.KeyCode == Keys.Enter)
+            {
+                AddTask();
+            }
+        }
+
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            if(tasksListBox.SelectedIndex >= 0 && !string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                tasksListBox.Items[tasksListBox.SelectedIndex] = textBox.Text;
+                textBox.Clear();
+                MessageBox.Show("Task edited successfully", "Conformation message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if(string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                MessageBox.Show("Input is empty");
+            }
+            else
+            {                MessageBox.Show("Select a task that you want to edit.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tasksListBox.DrawMode = DrawMode.OwnerDrawFixed;
+            tasksListBox.DrawItem += new DrawItemEventHandler(TasksListBox_DrawItem);
+            tasksListBox.MouseDoubleClick += new MouseEventHandler(TasksListBox_MouseDoubleClick);
+        }
+
+        private void TasksListBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0)
+                return;
+
+
+            string itemText = tasksListBox.Items[e.Index].ToString();
+            Font font = crossedOutItems.Contains(e.Index) ? new Font(e.Font, FontStyle.Strikeout) : e.Font;
+
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+            {              
+                e.Graphics.FillRectangle(Brushes.LightSeaGreen, e.Bounds);        
+            }
+            else
+            {               
+                e.Graphics.FillRectangle(Brushes.White, e.Bounds);  
+            }
+
+            e.Graphics.DrawString(itemText, font, Brushes.Black, e.Bounds);
+            e.DrawFocusRectangle();
+        }
+
+        private void TasksListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (doubleClickHandled)
+                return;
+
+            doubleClickHandled = true;
+
+            int clickedIndex = tasksListBox.IndexFromPoint(e.Location);
+            if (clickedIndex != ListBox.NoMatches)
+            {
+                if (!crossedOutItems.Contains(clickedIndex))
+                {
+                    crossedOutItems.Add(clickedIndex);
+                }
+                else
+                {
+                    crossedOutItems.Remove(clickedIndex);
+                }
+                tasksListBox.Invalidate();
+            }
+            Task.Delay(200).ContinueWith(_ => doubleClickHandled = false);
+        }
+    }
+}
